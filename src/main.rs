771457294f0ca822e803c622647bd1785b1d28a2
Kardev07 <77888951+Kardev07@users.noreply.github.com>
2022-02-07 12:28:49 +0530

    chore: include better practices for writing the data provided by the user and stored by the server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tokio::{io::{AsyncReadExt, AsyncWriteExt} ,net::TcpListener};
+use tokio::{io::{AsyncWriteExt, BufReader, AsyncBufReadExt} ,net::TcpListener};
 
 #[tokio::main]
 async fn main() {
@@ -8,15 +8,26 @@ async fn main() {
     // variable containing the socket and address which accepts the incoming connection
     let (mut socket, _addr) = listener.accept().await.unwrap();
 
+    let (reader, mut writer) = socket.split();
+
+    // buffer that reads and stores data from the network stream
+    let mut reader = BufReader::new(reader);
+    
+    // the data that the user provides
+    let mut line = String::new();
     // running an infinite loop enabling to read and write more than 1 message
     loop {
-        // buffer to read data from the network stream
-        let mut buffer = [0u8; 1024];
-    
-        /* read and store the data that the user provides in the network stream, eg: user types "hello", the data is read and stored */
-        let bytes_read = socket.read(&mut buffer).await.unwrap();
+        /* the bytes that the server read from the data that the user provided */
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        if bytes_read == 0 {
+            break;
+        }
     
         /* writing the data read back to the user. the data read and stored before is sent back to the user which in this case is "hello" */ 
-        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+        writer.write_all(line.as_bytes()).await.unwrap();
+
+        /* clears the line after its been printed back by the server to prevent the present line being printed again when the next message 
+        needs to be printed */
+        line.clear();
     }
 }
