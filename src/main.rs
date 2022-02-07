ed642798fab948e5c9f4224dfe820b2c66db48dd
Kardev07 <77888951+Kardev07@users.noreply.github.com>
2022-02-07 12:10:28 +0530

    chore: complete basic functionality of server reading and storing data from the user, and writing it back to the user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{io::{AsyncReadExt, AsyncWriteExt} ,net::TcpListener};
+
+#[tokio::main]
+async fn main() {
+    // listener which listens for any upcoming connections
+    let listener = TcpListener::bind("localhost:8000").await.unwrap();
+
+    // variable containing the socket and address which accepts the incoming connection
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+
+    // running an infinite loop enabling to read and write more than 1 message
+    loop {
+        // buffer to read data from the network stream
+        let mut buffer = [0u8; 1024];
+    
+        /* read and store the data that the user provides in the network stream, eg: user types "hello", the data is read and stored */
+        let bytes_read = socket.read(&mut buffer).await.unwrap();
+    
+        /* writing the data read back to the user. the data read and stored before is sent back to the user which in this case is "hello" */ 
+        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+    }
 }
